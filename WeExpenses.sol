d09e10c8a881cbd1e35776db39f60966a65a74f6
whathewatt <adrien.arcuri.pro@gmail.com>
2018-02-22 14:51:34 +0100

    Add get maxBalance

--- WeExpenses.sol
+++ WeExpenses.sol
@@ -7,6 +7,7 @@ contract WeExpenses {
         string name; // name of the participant or the organization
         address waddress; // address of the participant
         int balance; // balance of the participant (int type because balance can be negative)
+        uint index; // index of the participant in the address list
     }
 
     // Expense which will be part of all the expenses
@@ -31,6 +32,9 @@ contract WeExpenses {
     // stores a `Participant` struct for each possible address.
     mapping(address => Participant) public participants;
 
+    address[] public addressList;
+    
+
     // A dynamically-sized array of `Expenses` structs.
     Expense[] public expenses;
 
@@ -51,7 +55,8 @@ contract WeExpenses {
 
     // Create a new participant in the participants mapping
     function createParticipant(string name, address waddress) public {
-        Participant memory participant = Participant({name: name, waddress: waddress, balance: 0});
+        Participant memory participant = Participant({name: name, waddress: waddress, balance: 0, index: 0});
+        participant.index = addressList.push(waddress)-1; //add the address to the addressList
         participants[waddress] = participant;
     }
 
@@ -68,7 +73,20 @@ contract WeExpenses {
         participants[expense.payBy].balance += int(expense.amount);
         for (uint i = 0; i < expense.payFor.length; i++) {
                 participants[expense.payFor[i]].balance -= int(portion);
-        }        
+        }       
+    }
+
+    // Return maximum balance and the index of the list of participants
+    function getMaxBalance() public view returns (int, uint) {
+        int max = participants[addressList[0]].balance;
+        uint index = 0;
+        for (uint i = 1; i < addressList.length; i++) {
+            if(max != max256(max, participants[addressList[i]].balance)) {
+                max=participants[addressList[i]].balance;
+                index = i;
+            }
+        }
+        return (max,index);
     }
   
     // Synchronize the balance after each new refund
@@ -81,5 +99,13 @@ contract WeExpenses {
     function getExpensePayFor(uint i) public view returns (address[]){
       return expenses[i].payFor;
     }
-    
+
+    function max256(int256 a, int256 b) internal pure returns (int256) {
+        return a >= b ? a : b;
+    }
+
+    function min256(int256 a, int256 b) internal pure returns (int256) {
+        return a < b ? a : b;
+    }    
 }
+    
