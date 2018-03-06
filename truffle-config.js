acbecc8e7e5d8da8bb696b8d138e64d0594ddc5d
Adrien Arcuri <adrien.arcuri.pro@gmail.com>
2018-03-06 22:56:08 +0100

    Update configuration to use expectThrow

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,8 @@
 
 // Allows us to use ES6 in our migrations and tests.
-require('babel-register');
+require('babel-register')({
+  ignore: /node_modules\/(?!zeppelin-solidity\/test\/helpers)/
+});
 require('babel-polyfill');
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
--- truffle.js
+++ truffle.js
@@ -1,4 +1,3 @@
-
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')({
   ignore: /node_modules\/(?!zeppelin-solidity\/test\/helpers)/
@@ -13,5 +12,11 @@ module.exports = {
       port: 8545,
       network_id: '*' // Match any network id
     }
+  },
+  coverage: {
+    host: "localhost",
+    port: 8555,
+    network_id: "*",
+    gas: 0xffffffff
   }
 }
